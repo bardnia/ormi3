ad61609dfd2ff5ddbe9198ca08c4844db5f5747a
bardnia <69304793+bardnia@users.noreply.github.com>
2023-10-05 23:33:53 +0900

    Add files via upload

---
+++ project/BLOG/mysite/tutorialdjango/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from main.views import index
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('main.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('blog/', include('blog.urls')),
+]
