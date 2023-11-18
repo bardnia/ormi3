f55315f0d85f60a2fcc840e1ba4fd496173edd54
bardnia <69304793+bardnia@users.noreply.github.com>
2023-11-18 23:48:49 +0900

    Add files via upload

---
+++ djang/JWT/testrequest.py
@@ -0,0 +1,45 @@
+# pip install requests
+import requests
+
+HOST = 'http://localhost:8000'
+LOGIN_URL = HOST + '/account/login/'
+
+
+# 로그인
+# 사용자가 본 html파일에 입력 form에서 입력한 데이터라고 생각해주세요. 
+LOGIN_INFO = {
+    "email": "ju3099314@naver.com",
+    "password": "heeju4649"
+}
+
+# 로그인을 위해 post요청을 보냅니다.
+response = requests.post(LOGIN_URL, data=LOGIN_INFO)
+print(response.status_code)
+print(response.text)
+print(response.json())
+print(response.json()['access_token'])
+
+token = response.json()['access_token']
+
+
+# # 로그인한 사용자만 들어갈 수 있는 URL에 접속
+# # headers에 token을 넣어서 보냅니다.
+# header = {
+#     'Authorization': 'Bearer ' + token
+# }
+
+# step2 로그인한 사용자만 들어갈 수 있는 URL에 접속
+# headers에 token을 넣어서 보냅니다.
+header = {
+    'Authorization': 'Bearer ' + token
+}
+
+data = {
+    'title': '제목',
+    'content': '내용',
+    'author': 1
+}
+
+res = requests.get(HOST + '/account/test/', headers=header, data=data)
+print(res.status_code)
+print(res.text)
